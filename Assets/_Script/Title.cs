0bc0661c3ca8b73e5a2e5f68ead949837da90630
Xinghao She <xinghao.she@stereye.com>
2021-04-15 00:51:17 +0800

    init

---
+++ Assets/_Script/Title.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Title : MonoBehaviour
+{
+    public GameObject MainView;
+
+
+    public void OnStartClick()
+    {
+        MainView = (GameObject) Instantiate(Resources.Load("Prefab/Main"));
+        MainView.name = "Main";
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
